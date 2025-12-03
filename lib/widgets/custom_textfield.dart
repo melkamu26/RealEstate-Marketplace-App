44de18416b60b815bd87ed5ed5a5539cb0059ea0
melkamu26 <melkamugebre8@gmail.com>
2025-12-02 19:29:32 -0500

    feat: add reusable custom textfield and button widgets

---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hint;
+  final bool obscure;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hint,
+    this.obscure = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscure,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: hint,
+      ),
+    );
+  }
+}
